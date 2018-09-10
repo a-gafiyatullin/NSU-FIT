69fc94966d682e8db5d02abe5d172d1cf672b7e6
Albert Gafiyatullin <binixary@gmail.com>
2018-09-10 19:39:18 +0700

    the solution for the laboratory work #1 was added CaD

--- The 2nd course/Computers and devices/laboratory work #1/main.cpp
+++ The 2nd course/Computers and devices/laboratory work #1/main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 template<typename T>
 void bubble_sort(vector<T> &v)
 {
-        for(auto out_iter = v.end(); out_iter != v.begin(); out_iter--)
+        for(auto out_iter = v.end() - 1; out_iter != v.begin(); out_iter--)
                 for(auto in_iter = v.begin(); in_iter != out_iter; in_iter++)
                         if(*in_iter > *(in_iter + 1))
                                 swap(*in_iter, *(in_iter + 1));
@@ -30,8 +30,7 @@ int main()
         bubble_sort(v);
         times(&finish);
         double clocks = finish.tms_utime - start.tms_utime;
-        cout << "Total process time: " << (double)clocks / clocks_per_sec
+        cout << endl << "Total process time: " << (double)clocks / clocks_per_sec
                 << "s" << endl;
-
         return 0;
 }
