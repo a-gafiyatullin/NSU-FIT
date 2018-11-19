a0cb620b8c69676b57b04a246e5e8fba2b5da7ef
Albert Gafiyatullin <binixary@gmail.com>
2018-11-19 20:45:55 +0700

    CaD LW№7: added some functions for working with matrices, but nothing work yet :(

--- The 2nd course/Computers and devices/laboratory work #7/without_vectorization.c
+++ The 2nd course/Computers and devices/laboratory work #7/without_vectorization.c
@@ -1,84 +1,86 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-struct matrix {
-    float** matrix_;
-    int order_;
-    int l1Norm_;
-    int maxNorm_;
-};
+#include "matrix.h"
 
 struct matrix* create_matrix(int order){
     struct matrix* m = malloc(sizeof(struct matrix));
     m->order_ = order;
-    m->l1Norm_ = 0;
     m->matrix_ = malloc(sizeof(float*) * order);
     for(int i = 0; i < order; i++)
         m->matrix_[i] = malloc(sizeof(float) * order);
     return m;
 }
 
-void get_matrix(struct matrix* m, FILE *in){
-    for(int i = 0; i < m->order_; i++){
-        int currentNorm = 0;
-        for(int j = 0; j < m->order_; j++){
+void get_matrix(struct matrix* m, FILE* in){
+    for(int i = 0; i < m->order_; i++)
+        for(int j = 0; j < m->order_; j++)
             fscanf(in, "%f", &m->matrix_[i][j]);
-            currentNorm += m->matrix_[i][j];
-        }
-        if(currentNorm > m->l1Norm_)
-            m->l1Norm_ = currentNorm;
-    }
 }
 
-void print_matrix(struct matrix* m, FILE *out){
+struct matrix* get_zero_matrix(int order){
+    struct matrix* m = malloc(sizeof(struct matrix));
+    m->order_ = order;
+    m->matrix_ = malloc(sizeof(float*) * order);
+    for(int i = 0; i < order; i++)
+        m->matrix_[i] = calloc(order, sizeof(float));
+    return m;
+}
+
+struct matrix* get_identity_matrix(int order){
+    struct matrix* m = malloc(sizeof(struct matrix));
+    m->order_ = order;
+    m->matrix_ = malloc(sizeof(float*) * order);
+    for(int i = 0; i < order; i++){
+        m->matrix_[i] = calloc(order, sizeof(float));
+        m->matrix_[i][i] = 1;
+    }
+    return m;
+}
+
+void print_matrix(struct matrix* m, FILE* out){
     for(int i = 0; i < m->order_; i++){
         for(int j = 0; j < m->order_; j++)
-            fprintf(out, "%.2f ", m->matrix_[i][j]);
+            fprintf(out, "%.4f ", m->matrix_[i][j]);
         fprintf(out, "\n");
     }
 }
 
-struct matrix* transpose_matrix(struct matrix* m){
-    struct matrix* tmp = create_matrix(m->order_);
-    for(int i = 0; i < m->order_; i++){
-        int currentNorm = 0;
-        for(int j = 0; j < m->order_; j++){
-            tmp->matrix_[i][j] = m->matrix_[j][i];
-            currentNorm += m->matrix_[j][i];
-        }
-        if(currentNorm > m->maxNorm_)
-            m->maxNorm_ = currentNorm;
-    }
-    tmp->l1Norm_ = m->maxNorm_;
-    tmp->maxNorm_ = m->l1Norm_;
-    return tmp;
+void free_matrix(struct matrix* m){
+    for(int i = 0; i < m->order_; i++)
+        free(m->matrix_[i]);
+    free(m->matrix_);
+    free(m);
 }
 
-void sum_matrix(struct matrix* a, struct matrix* b){
+void copy_matrix(struct matrix* dst, struct matrix* src){
+    for(int i = 0; i < src->order_; i++)
+        memcpy(dst->matrix_[i], src->matrix_[i], sizeof(float) * src->order_);
+}
+
+void transpose_matrix(struct matrix* m){
+    for(int i = 0; i < m->order_; i++)
+        for(int j = i; j < m->order_; j++){
+            float tmp = m->matrix_[i][j];
+            m->matrix_[i][j] = m->matrix_[j][i];
+            m->matrix_[j][i] = tmp;
+    }
+}
+
+void sum_matrices(struct matrix* a, struct matrix* b){
     for(int i = 0; i < a->order_; i++)
         for(int j = 0; j < a->order_; j++)
             a->matrix_[i][j] += b->matrix_[i][j];
 }
 
-void sub_matrix(struct matrix* a, struct matrix* b){
+void sub_matrices(struct matrix* a, struct matrix* b){
     for(int i = 0; i < a->order_; i++)
         for(int j = 0; j < a->order_; j++)
             a->matrix_[i][j] -= b->matrix_[i][j];
 }
 
-struct matrix* mul_matrix(struct matrix* a, struct matrix* b, struct matrix* rez){
+struct matrix* mul_matrices(struct matrix* a, struct matrix* b){
+    struct matrix* tmp = get_zero_matrix(a->order_);
     for(int i = 0; i < a->order_; i++)
         for(int j = 0; j < a->order_; j++)
             for(int k = 0; k < a->order_; k++)
-                rez->matrix_[i][j] += a->matrix_[i][k] * b->matrix_[k][j];
-}
-
-int main(){
-    struct matrix* m1 = create_matrix(3);
-    get_matrix(m1, stdin);
-    struct matrix* m2 = create_matrix(3);
-    get_matrix(m2, stdin);
-    sum_matrix(m1, m2);
-    print_matrix(m1, stdout);
-    return 0;
+                tmp->matrix_[i][j] += a->matrix_[i][k] * b->matrix_[k][j];
+    return tmp;
 }
