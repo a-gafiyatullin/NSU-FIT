ff1379f1061cadeed82f82368264ca190b3cc156
Albert Gafiyatullin <binixary@gmail.com>
2020-03-09 00:20:06 +0700

    Fix repository for case-insensitive filesystems.

--- the-3rd-year/network-programming/laboratory_work_#2/server/server.cpp
+++ the-3rd-year/network-programming/laboratory_work_#2/server/server.cpp
@@ -1,52 +1,54 @@
 #include "Server.h"
-#include <csignal>
-#include <iostream>
-#include <unistd.h>
 
-bool stop = false;
+Server::Server(in_port_t port)
+    : port(port), socket(::socket(AF_INET, SOCK_STREAM, 0)) {
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_port = port;
+  serverAddress.sin_addr.s_addr = INADDR_ANY;
+}
 
-void do_stop(int) { stop = true; }
-
-int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    std::cerr << "usage: server [port]" << std::endl;
-    return -1;
-  }
-  in_port_t port = atoi(argv[1]);
-  if (port < 1024) {
-    std::cerr << "port must be >= 1024" << std::endl;
-    return -1;
+Server::~Server() {
+  for (auto t : clientThreads) {
+    pthread_join(*t.first, nullptr);
+    delete t.first;
+    delete t.second;
   }
 
-  if (chdir("uploads") < 0) {
-    if (mkdir("uploads", S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) < 0) {
-      std::perror("mkdir");
-      return -1;
-    }
-    if (chdir("uploads") < 0) {
-      std::perror("chdir");
-      return -1;
-    }
-  }
+  close(socket);
+}
 
-  struct sigaction act {};
-  act.sa_handler = do_stop;
-  sigemptyset(&act.sa_mask);
-  act.sa_flags = ~SA_RESTART;
-  if (sigaction(SIGINT, &act, nullptr) < 0) {
-    std::perror("sigaction");
+int Server::bind() {
+  if (socket < 0) {
     return -1;
   }
 
-  Server server(port);
-  if (server.bind() < 0) {
-    std::perror("Server::bind");
+  if (::bind(socket, (sockaddr *)(&serverAddress), sizeof(serverAddress)) < 0) {
     return -1;
   }
 
-  while (!stop) {
-    server.accept();
+  if (listen(socket, SOMAXCONN) < 0) {
+    return -1;
   }
+
+  return 0;
+}
+
+int Server::accept() {
+  sockaddr_in client_addr{};
+  socklen_t addr_len = sizeof(client_addr);
+  int clientSocket = ::accept(socket, (sockaddr *)(&client_addr), &addr_len);
+  if (clientSocket < 0) {
+    return -1;
+  }
+  auto *client_info = new ClientProcessing(clientSocket);
+  auto *client_thread = new pthread_t;
+  if (pthread_create(client_thread, nullptr,
+                     &ClientProcessing::clientProcessing,
+                     (void *)client_info) < 0) {
+    return -1;
+  }
+
+  clientThreads.emplace_back(client_thread, client_info);
 
   return 0;
 }
