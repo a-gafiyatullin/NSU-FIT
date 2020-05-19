b0e29f361d6bc73ca62c5905f12154e8613e5e67
Albert Gafiyatullin <binixary@gmail.com>
2020-05-19 14:58:06 +0700

    DB: added UI for show editing.

---
+++ the-3rd-year/data-bases/pl-sql/theater_info_system/src/main/java/ru/nsu/ShowEditing.java
@@ -0,0 +1,47 @@
+package ru.nsu;
+
+import javax.swing.*;
+
+public class ShowEditing {
+    private JComboBox titlesComboBox;
+    private JButton editTitleButton;
+    private JButton addTitleButton;
+    private JFormattedTextField titleCenturyTextField;
+    private JComboBox directorsComboBox;
+    private JComboBox productionDesignersComboBox;
+    private JComboBox conductorsComboBox;
+    private JComboBox authorsComboBox;
+    private JComboBox agesComboBox;
+    private JTextField titleNameTextField;
+    private JComboBox genresComboBox;
+    private JButton saveTitleButton;
+    private JFormattedTextField premierDateTextField;
+    private JComboBox characteristics;
+    private JTable characteristicsTable;
+    private JFormattedTextField characteristicValue;
+    private JButton saveRoleButton;
+    private JButton addRoleButton;
+    private JButton deleteTitleButton;
+    private JTextField roleNameTextField;
+    private JComboBox rolesComboBox;
+    private JButton editRoleButton;
+    private JButton deleteRoleButton;
+    private JButton addCharacteristicButton;
+    private JButton deleteCharacteristicButton;
+    private JCheckBox isMainRoleCheckBox;
+    private JComboBox actorsComboBox;
+    private JButton addAsUnderstudyActorButton;
+    private JButton addAsMainActorButton;
+    private JComboBox instrumentsComboBox;
+    private JTable musiciansTable;
+    private JButton addMusicianToTitleButton;
+    private JButton deleteMusicianFromTitleButton;
+    private JTable table3;
+    private JFormattedTextField dateTextField;
+    private JButton deleteDateButton;
+    private JButton addDateButton;
+    private JLabel status;
+    private JPanel mainPanel;
+    private JComboBox rolesForActorsComboBox;
+    private JScrollPane performancesTable;
+}
